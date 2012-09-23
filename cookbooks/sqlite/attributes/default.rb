247542a1623facaa49abfefdf7068ba5f5b9745d
Jon Wong <j@jnwng.com>
2012-09-23 01:05:25 -0400

    Adding sqlite cookbook

---
+++ cookbooks/sqlite/attributes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: sqlite
+# Attributes:: default
+#
+# Author:: Jon Wong <j@jnwng.com>
+#
+
+# SQLite versions go like 3XXYYZZ
+# So 3.7.4 = 3071400
+default['sqlite']['version'] = '3071400'
+default['sqlite']['checksum'] = '6464d429b1396a8db35864e791673b65'
+
