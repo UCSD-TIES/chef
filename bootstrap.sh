cf085d22b53f610657d76672c7dcce0d2538a183
Jon Wong <j@jnwng.com>
2012-11-03 02:44:40 -0700

    Adding vagrant bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,11 @@
 if [ "$JENKINS" ] 
 then
     NODE="node-jenkins.json"
+    CONFIG="./solo-jenkins.rb"
 else
     NODE="node.json"
+    CONFIG="./solo.rb"
 fi
 
-sudo chef-solo -c ./solo.rb -j $NODE
+sudo chef-solo -c $CONFIG -j $NODE
 exit
