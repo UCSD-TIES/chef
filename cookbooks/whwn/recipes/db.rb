e8e0d506d091e342da3983571dad9843be99bd31
Jon Wong <j@jnwng.com>
2012-11-03 21:23:39 -0700

    Putting pg stuff in db.rb

--- cookbooks/whwn/recipes/db.rb
+++ cookbooks/whwn/recipes/db.rb
@@ -8,3 +8,14 @@ include_recipe "postgresql::server"
 include_recipe "postgresql::contrib"
 include_recipe "postgresql::libpq"
 include_recipe "postgresql::postgis"
+
+pg_user "whwn" do
+  privileges :superuser => true, :createdb => false, :login => true
+  password "whwn2012"
+end
+
+pg_database "whwn" do
+  owner "whwn"
+  encoding "utf8"
+  template "template_postgis"
+end
--- cookbooks/whwn/recipes/default.rb
+++ cookbooks/whwn/recipes/default.rb
@@ -22,17 +22,6 @@ package "mercurial"
 include_recipe "git::source"
 include_recipe "python"
 
-pg_user "whwn" do
-  privileges :superuser => true, :createdb => false, :login => true
-  password "whwn2012"
-end
-
-pg_database "whwn" do
-  owner "whwn"
-  encoding "utf8"
-  template "template_postgis"
-end
-
 directory node['whwn']['virtualenv'] do
   owner "whwn"
   group "whwn"
